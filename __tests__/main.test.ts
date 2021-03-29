efe0fe11f881f463dc1be8c2b410354c09de1178
Jacob Carlborg <doob@me.com>
2021-03-29 20:37:42 +0200

    Some fixes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,7 +6,7 @@ test('extractIpAddress - finding IP address', () => {
   const arpOutput = [
     '? (0.0.0.0) at 00:00:00:00:00:00 on en2 ifscope [ethernet]',
     `? (${ipAddress}) at ${macAddress} on en1 ifscope [ethernet]`
-  ].join("\n")
+  ].join('\n')
   const vm = new main.XhyveVm()
 
   expect(vm.extractIpAddress(arpOutput, macAddress)).toBe(ipAddress)
@@ -16,8 +16,8 @@ test('extractIpAddress - not finding IP address', () => {
   const macAddress = '40:8e:71:34:88:eb'
   const arpOutput = [
     '? (0.0.0.0) at 00:00:00:00:00:00 on en2 ifscope [ethernet]',
-    '? (0.0.0.1) at 00:00:00:00:00:01 on en1 ifscope [ethernet]',
-  ].join("\n")
+    '? (0.0.0.1) at 00:00:00:00:00:01 on en1 ifscope [ethernet]'
+  ].join('\n')
   const vm = new main.XhyveVm()
 
   expect(vm.extractIpAddress(arpOutput, macAddress)).toBe(undefined)
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import * as path from 'path'
 import * as fs from 'fs'
 import * as exec from '@actions/exec'
 import {spawn} from 'child_process'
-import { resourceLimits } from 'worker_threads'
 
 async function execWithOutput(
   commandLine: string,
@@ -12,7 +11,7 @@ async function execWithOutput(
 ): Promise<string> {
   let output!: string
 
-  const exitCode = await exec.exec('./run.sh', args, {
+  const exitCode = await exec.exec(commandLine, args, {
     listeners: {
       stdout: buffer => (output += buffer.toString())
     }
@@ -24,40 +23,100 @@ async function execWithOutput(
   return output
 }
 
+interface VmOptions {
+  memory: string
+  uuid: string
+  diskImage: fs.PathLike
+  cpuCount: number
+  userboot: fs.PathLike
+}
+
+const enum VmType {
+  freeBsd
+}
+
 export class XhyveVm {
   macAddress!: string
   ipAddress!: string
+  private sshKey: fs.PathLike
+  private xhyvePath: fs.PathLike
+  protected options: VmOptions
+
+  constructor(sshKey: fs.PathLike, xhyvePath: fs.PathLike, options: VmOptions) {
+    this.sshKey = sshKey
+    this.xhyvePath = xhyvePath
+    this.options = options
+  }
+
+  static getVm(type: VmType): typeof XhyveVm {
+    switch (type) {
+      case VmType.freeBsd:
+        return FreeBsd
+    }
+  }
 
   async init(): Promise<void> {
     this.macAddress = await this.getMacAddress()
   }
 
   run(): void {
-    spawn('./run.sh', {detached: true})
+    spawn('sudo', this.xhyveArgs, {detached: true})
   }
 
   stop(): void {
-    this.execute('shutdown -h -ph now')
+    this.execute('shutdown -h -p now')
   }
 
   async execute(command: string): Promise<void> {
     const buffer = Buffer.from(command)
-    await exec.exec('ssh', ['-i', 'id_ed25519', `root@${this.ipAddress}`], {
-      input: buffer
-    })
+    await exec.exec(
+      'ssh',
+      ['-i', this.sshKey.toString(), `root@${this.ipAddress}`],
+      {
+        input: buffer
+      }
+    )
   }
 
   async getMacAddress(): Promise<string> {
-    return (this.macAddress = await execWithOutput('./run.sh'))
+    return (this.macAddress = await execWithOutput(
+      'sudo',
+      this.xhyveArgs.concat('-M')
+    ))
   }
 
   extractIpAddress(arpOutput: string, macAddress: string): string | undefined {
     const result = arpOutput
-      .split("\n")
+      .split('\n')
       .find(e => e.includes(macAddress))
-      ?.match(/\((.+)\)/);
+      ?.match(/\((.+)\)/)
 
-    return result ? result[1] : undefined;
+    return result ? result[1] : undefined
+  }
+
+  get xhyveArgs(): string[] {
+    // prettier-ignore
+    return [
+      this.xhyvePath.toString(),
+      '-U', this.options.uuid,
+      '-A',
+      '-H',
+      '-m', this.options.memory,
+      '-c', this.options.cpuCount.toString(),
+      '-s', '0:0,hostbridge',
+      '-s', '2:0,virtio-net',
+      '-s', `4:0,virtio-blk,${this.options.diskImage}`,
+      '-s', '31,lpc',
+      '-l', 'com1,stdio'
+    ]
+  }
+}
+
+class FreeBsd extends XhyveVm {
+  get xhyveArgs(): string[] {
+    return super.xhyveArgs.concat(
+      `-f fbsd,${this.options.userboot},${this.options.diskImage},`
+    )
   }
 }
 
@@ -65,14 +124,24 @@ class Action {
   private readonly resourceUrl =
     'https://github.com/jacob-carlborg/xhyve-test/releases/download/qcow2/resources.tar'
 
+  private readonly targetDiskName = 'disk.raw'
+
   async run(): Promise<void> {
     const resourcesArchivePath = await this.downloadResources()
     const resourcesDirectory = await this.unarchiveResoruces(
       resourcesArchivePath
     )
-    this.configSSH()
-    await this.convertToRawDisk()
-    const vm = new XhyveVm()
+    const sshKeyPath = path.join(resourcesDirectory, 'id_ed25519')
+    this.configSSH(sshKeyPath)
+    await this.convertToRawDisk(resourcesDirectory)
+    const VmClass = XhyveVm.getVm(VmType.freeBsd)
+    const vm = new VmClass(sshKeyPath, path.join(resourcesDirectory, 'xhyve'), {
+      memory: '4G',
+      cpuCount: 2,
+      diskImage: path.join(resourcesDirectory, this.targetDiskName),
+      uuid: '864ED7F0-7876-4AA7-8511-816FABCFA87F',
+      userboot: path.join(resourcesDirectory, 'userboot.so')
+    })
     await vm.init()
     vm.run()
   }
@@ -84,42 +153,45 @@ class Action {
 
   async unarchiveResoruces(resourcesArchivePath: string): Promise<string> {
     core.info(`Unarchiving resoruces: ${resourcesArchivePath}`)
-    return cache.extractTar(resourcesArchivePath)
+    return cache.extractTar(resourcesArchivePath, undefined, '-x')
   }
 
-  configSSH(): void {
+  configSSH(sshKey: fs.PathLike): void {
     const homeDirectory = process.env['HOME']
 
-    if (homeDirectory === undefined) throw Error('Failed to get the home direcory')
+    if (homeDirectory === undefined)
+      throw Error('Failed to get the home direcory')
 
     const sshDirectory = path.join(homeDirectory, '.ssh')
+    fs.mkdirSync(sshDirectory, {mode: 0o700})
     fs.appendFileSync(
       path.join(sshDirectory, 'config'),
       'StrictHostKeyChecking=accept-new'
     )
-    fs.chmodSync('id_ed25519', '0o600')
-    fs.chmodSync(sshDirectory, '0o600')
+
+    fs.chmodSync(sshKey, 0o600)
   }
 
-  async convertToRawDisk(): Promise<void> {
-    await exec.exec('./qemu-img', [
+  async convertToRawDisk(resourcesDirectory: fs.PathLike): Promise<void> {
+    const resDir = resourcesDirectory.toString()
+    await exec.exec(path.join(resDir, 'qemu-img'), [
       'convert',
       '-f',
       'qcow2',
       '-O',
       'raw',
-      'disk.qcow2',
-      'disk.raw'
+      path.join(resDir, 'disk.qcow2'),
+      path.join(resDir, this.targetDiskName)
     ])
   }
 }
 
 async function main(): Promise<void> {
   try {
-    new Action().run()
+    await new Action().run()
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-//main()
+main()
